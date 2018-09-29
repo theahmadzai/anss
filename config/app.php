3ce7fdf17d5e7bfbbe67d9492ea56f24ec695ad0
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-09-29 14:54:48 +0500

    set app name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'ANSS'),
 
     /*
     |--------------------------------------------------------------------------
@@ -156,9 +156,9 @@ return [
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // App\Providers\BroadcastServiceProvider::class,
 
     ],
 
