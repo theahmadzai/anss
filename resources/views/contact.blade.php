379da79240aa6a37886c91001d80212aea1619c7
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-21 03:46:45 +0500

    updated

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,8 +4,42 @@
 
 @section('content')
 
+    <div class="bar">
+        <h2>Contact Us</h2>
+    </div>
+
     <div class="container">
-        <h1>Hello World</h1>
+        <div class="container__narrow"></div>
+        <div class="container__wide">
+            <div class="container__row">
+
+                <h1>Contact Form</h1>
+
+                <p>Thank you for using this form to leave us a message. Required fields marked with *.</p>
+
+                <form action="">
+                    <div class="form__control">
+                        <label for="">Name <span>*</span></label>
+                        <input type="text">
+                    </div>
+                    <div class="form__control">
+                        <label for="">E-mail Address <span>*</span></label>
+                        <input type="email">
+                    </div>
+                    <div class="form__control">
+                        <label for="">Subject <span>*</span></label>
+                        <input type="text">
+                    </div>
+                    <div class="form__control">
+                        <label for="">Message <span>*</span></label>
+                        <textarea name=""></textarea>
+                    </div>
+                    <div class="form__control">
+                        <button>Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
 @endsection
