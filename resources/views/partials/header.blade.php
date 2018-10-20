abf12f383057d606d7a03274921a4b9c3846ff0b
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-21 03:46:19 +0500

    updated

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,4 +4,10 @@
     <a href="/" class="logo">
         <img src="{{ asset('img/logo.jpg') }}" alt="Afghan Network for Social Services" width="100" height="100">
     </a>
+
+    <div style="padding:0 2rem;">
+        <a href="#"><span class="icon icon-twitter"></span></a>
+        <a href="#"><span class="icon icon-facebook"></span></a>
+        <a href="#"><span class="icon icon-instagram"></span></a>
+    </div>
 </header>
