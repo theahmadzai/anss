5b4ac817871dcc87173bfe7cabe1b1c56b09298c
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-21 03:46:32 +0500

    updated

---
+++ resources/views/news.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('title', $article->title)
+
+@section('content')
+
+    <div class="bar">
+        <h2>{{ $article->title }}</h2>
+    </div>
+
+    <div class="container">
+        <div class="container__wide">
+            <div class="container__row">
+                <article class="flex flex--column article">
+                    <div class="flex__item"><span class="icon icon-calendar"></span><span>{{ $article->created_at }}</span></div>
+                    <div class="flex__item"><span class="icon icon-price-tag"></span>{{ $article->tags }}</div>
+                    <div class="flex__item"><p>{{ $article->content }}</p></div>
+                    <div class="flex__item"><a href="#" class="button">Read More</a></div>
+                </article>
+            </div>
+        </div>
+
+        <div class="container__narrow"></div>
+    </div>
+
+@endsection
