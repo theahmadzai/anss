44861233b13b94d906cf2150ddabe2b72e9ffd5c
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-23 01:33:13 +0500

    fixed

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,8 +17,7 @@
             @include('partials.footer')
         <div>
 
-        {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> --}}
-        <script src="{{ asset('js/libs/jquery.js') }}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
