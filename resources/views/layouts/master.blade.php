b625fce025a9bb8c3c69ee0ea615b5cf965dbf7e
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-23 01:50:58 +0500

    Updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,6 +18,7 @@
         <div>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
