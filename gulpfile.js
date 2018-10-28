02f857a5c4a92731d44c24c3f0f9d0654c1019ad
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-28 18:16:34 +0500

    add prod and dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const gulp = require('gulp')
 const sourcemaps = require('gulp-sourcemaps')
+const uglify = require('gulp-uglify')
 const postcss = require('gulp-postcss')
 const rollup = require('gulp-better-rollup')
 const babel = require('rollup-plugin-babel')
 const resolve = require('rollup-plugin-node-resolve')
 const browserSync = require('browser-sync')
 
-gulp.task('styles', () => {
+gulp.task('styles:prod', () => {
+  return gulp.src('./resources/styles/app.css')
+    .pipe(postcss())
+    .pipe(gulp.dest('./public/css'))
+})
+
+gulp.task('styles:dev', () => {
   return gulp.src('./resources/styles/app.css')
     .pipe(sourcemaps.init())
     .pipe(postcss())
@@ -15,7 +22,26 @@ gulp.task('styles', () => {
     .pipe(browserSync.reload({ stream: true }))
 })
 
-gulp.task('scripts', () => {
+gulp.task('scripts:prod', () => {
+  return gulp.src('./resources/scripts/app.js')
+    .pipe(rollup({
+      plugins: [
+        resolve(),
+        babel()
+      ],
+      external: [ 'jquery' ]
+    }, {
+      format: 'iife',
+      globals: {
+        jquery: '$'
+      }
+    }
+    ))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js'))
+})
+
+gulp.task('scripts:dev', () => {
   return gulp.src('./resources/scripts/app.js')
     .pipe(sourcemaps.init())
     .pipe(rollup({
@@ -36,7 +62,7 @@ gulp.task('scripts', () => {
     .pipe(browserSync.reload({ stream: true }))
 })
 
-gulp.task('serve', gulp.series('styles', 'scripts', () => {
+gulp.task('serve', gulp.series('styles:dev', 'scripts:dev', () => {
   browserSync({
     proxy: {
       target: 'http://127.0.0.1:8000'
@@ -53,12 +79,10 @@ gulp.task('dev', gulp.parallel( 'serve', () => {
     return gulp.src(path)
       .pipe(browserSync.reload({ stream: true }))
   })
-
-  gulp.watch('./resources/scripts/**/*.js', gulp.series('scripts'))
-
-  gulp.watch('./resources/styles/**/*.css', gulp.series('styles'))
+  gulp.watch('./resources/styles/**/*.css', gulp.series('styles:dev'))
+  gulp.watch('./resources/scripts/**/*.js', gulp.series('scripts:dev'))
 }))
 
-gulp.task('prod', gulp.series('styles', 'scripts'))
+gulp.task('prod', gulp.series('styles:prod', 'scripts:prod'))
 
 gulp.task('default', gulp.series('dev'))
