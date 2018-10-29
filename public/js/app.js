8429fe3102599adc24b4013683a150a0c2710a14
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-29 23:43:12 +0500

    minified

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,1 @@
-(function ($$1) {
-  'use strict';
-
-  $$1 = $$1 && $$1.hasOwnProperty('default') ? $$1['default'] : $$1;
-
-  $$1.ajaxSetup({headers:{"X-CSRF-TOKEN":$$1("meta[name=\"csrf-token\"]").attr("content")}});
-
-  $("#top-line").css({width:"100%",height:"5px",background:"black"});
-
-}($));
-
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmpzIiwic291cmNlcyI6WyJyZXNvdXJjZXMvc2NyaXB0cy9jc3JmLmpzIiwicmVzb3VyY2VzL3NjcmlwdHMvYXBwLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAkIGZyb20gJ2pxdWVyeSdcblxuJC5hamF4U2V0dXAoe1xuICBoZWFkZXJzOiB7XG4gICAgJ1gtQ1NSRi1UT0tFTic6ICQoJ21ldGFbbmFtZT1cImNzcmYtdG9rZW5cIl0nKS5hdHRyKCdjb250ZW50JylcbiAgfVxufSlcbiIsImltcG9ydCAnLi9jc3JmJ1xuXG4kKCcjdG9wLWxpbmUnKS5jc3Moe1xuICB3aWR0aDogJzEwMCUnLFxuICBoZWlnaHQ6ICc1cHgnLFxuICBiYWNrZ3JvdW5kOiAnYmxhY2snXG59KVxuIl0sIm5hbWVzIjpbIiQiLCJhamF4U2V0dXAiLCJoZWFkZXJzIiwiYXR0ciIsImNzcyIsIndpZHRoIiwiaGVpZ2h0IiwiYmFja2dyb3VuZCJdLCJtYXBwaW5ncyI6Ijs7Ozs7QUFFQUEsS0FBQyxDQUFDQyxTQUFGLENBQVksQ0FDVkMsT0FBTyxDQUFFLENBQ1AsZUFBZ0JGLEdBQUMsQ0FBQywyQkFBRCxDQUFELENBQTZCRyxJQUE3QixDQUFrQyxTQUFsQyxDQURULENBREMsQ0FBWjs7RUNBQUgsQ0FBQyxDQUFDLFdBQUQsQ0FBRCxDQUFlSSxHQUFmLENBQW1CLENBQ2pCQyxLQUFLLENBQUUsTUFEVSxDQUVqQkMsTUFBTSxDQUFFLEtBRlMsQ0FHakJDLFVBQVUsQ0FBRSxPQUhLLENBQW5COzs7OyJ9
+!function(e){"use strict";(e=e&&e.hasOwnProperty("default")?e.default:e).ajaxSetup({headers:{"X-CSRF-TOKEN":e('meta[name="csrf-token"]').attr("content")}});if(0<$("#slider").length){var t=function(){var e=0<arguments.length&&void 0!==arguments[0]?arguments[0]:{},t=$(e.container),n=$(e.slider),i=n.children(),r=i.length,l=i.width(),a=i.height();t.css({width:l,height:a}),n.css({width:r*l});var c=function(){n.animate({left:+l},200,function(){n.children().last().prependTo(n),n.css({left:""})})},o=e.autoplay?setInterval(c,3e3):null;return(null!==o||void 0!==o)&&n.hover(function(){clearInterval(o)},function(){o=setInterval(c,3e3)}),{moveLeft:function(){n.animate({left:-l},200,function(){n.children().first().appendTo(n),n.css({left:""})})},moveRight:c}}({container:"#slider",slider:"#slider ul",autoplay:!0});$("#slider-prev").click(function(){t.moveLeft()}),$("#slider-next").click(function(){t.moveRight()})}$("#top-line").css({width:"100%",height:"2.5px",backgroundImage:"linear-gradient(to right, black, red, green)"})}($);
