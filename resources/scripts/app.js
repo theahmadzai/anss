bbd11085cb335b616432c3970ab860131aa91981
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-29 23:55:11 +0500

    added csrf code

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,9 +1,13 @@
-import './csrf'
 import './slider'
+
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+  }
+})
 
 $('#top-line').css({
   width: '100%',
   height: '2.5px',
   backgroundImage: 'linear-gradient(to right, black, red, green)'
 })
-
