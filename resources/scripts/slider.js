eac47ae709b5552317977d3986f8578bdca73703
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-10-30 07:43:46 +0500

    slider

--- resources/scripts/slider.js
+++ resources/scripts/slider.js
@@ -1,59 +1,74 @@
-const slider = function(options = {}) {
-  const container = $(options.container)
-  const slider = $(options.slider)
-  const slide = slider.children()
-  const slideLength = slide.length
-  const slideWidth = slide.width()
-  const slideHeight= slide.height()
+const slider = (function($) {
+  return function (options = {}) {
+    const container = $(options.container)
+    const slider = $(options.slider)
+    const slide = slider.children()
+    const image = $(options.image)
+    const slideLength = slide.length
+    const slideWidth = slide.width()
+    const slideHeight = slide.height()
 
-  container.css({
-    width: slideWidth,
-    height: slideHeight
-  })
+    const resize = () => {
+      console.log(slider.width())
+      image.css({
+        width: $(window).width(),
+        height: slideHeight
+      })
+    }
 
-  slider.css({
-    width: slideLength * slideWidth
-  })
-
-  const moveLeft = () => {
-    slider.animate({
-      left: - slideWidth
-    }, 200, () => {
-      slider.children().first().appendTo(slider)
-      slider.css({ left: '' })
+    slider.css({
+      width: slideLength * slideWidth,
+      marginLeft: -slideWidth
     })
-  }
 
-  const moveRight = () => {
-    slider.animate({
-      left: + slideWidth
-    }, 200, () => {
-      slider.children().last().prependTo(slider)
-      slider.css({ left: '' })
+    image.css({
+      width: slideWidth,
+      height: slideHeight
     })
+
+    const moveLeft = () => {
+      slider.animate({
+        left: + slideWidth
+      }, 1000, () => {
+        slider.children().last().prependTo(slider)
+        slider.css({ left: '' })
+      })
+    }
+
+    const moveRight = () => {
+      slider.animate({
+        left: - slideWidth
+      }, 1000, () => {
+        slider.children().first().appendTo(slider)
+        slider.css({ left: '' })
+      })
+    }
+
+    let interval = options.autoplay ? setInterval(moveRight, 3000) : null
+
+    if(interval !== null || interval !== undefined) {
+      slider.hover(() => {
+        clearInterval(interval)
+      }, () => {
+        interval = setInterval(moveRight, 3000)
+      })
+    }
+
+    return {
+      moveLeft,
+      moveRight,
+      resize
+    }
   }
 
-  let interval = options.autoplay ? setInterval(moveRight, 3000) : null
-
-  if(interval !== null || interval !== undefined) {
-    slider.hover(() => {
-      clearInterval(interval)
-    }, () => {
-      interval = setInterval(moveRight, 3000)
-    })
-  }
-
-  return {
-    moveLeft,
-    moveRight
-  }
-}
+})($)
 
 // HomePage Slider
 if( $('#slider').length > 0) {
   let homeSlider = slider({
     container: '#slider',
     slider: '#slider ul',
+    image: '#slider ul li img',
     autoplay: true
   })
 
@@ -64,4 +79,6 @@ if( $('#slider').length > 0) {
   $('#slider-next').click(() => {
     homeSlider.moveRight()
   })
+
+  window.addEventListener('resize', homeSlider.resize)
 }
