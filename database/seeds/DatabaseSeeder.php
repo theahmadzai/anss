bff7deabedef4db6d230d5968ba228b91299ddea
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-03 04:54:34 +0500

    add event seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            NewsTableSeeder::class
+            NewsTableSeeder::class,
+            EventsTableSeeder::class
         ]);
     }
 }
