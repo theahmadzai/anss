3109641b8dd40c006ae6c190dceab6b68370dd3c
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-03 05:00:58 +0500

    show list

--- resources/views/pages/events/upcoming-events.blade.php
+++ resources/views/pages/events/upcoming-events.blade.php
@@ -10,7 +10,20 @@
 
     <div class="container">
         <div class="container__narrow"></div>
-        <div class="container__wide"></div>
+
+        <div class="container__wide">
+            @foreach ($events as $event)
+                <div class="container__row">
+                    <article class="flex flex--column event">
+                        <div class="flex__item"><a href="/latest-news/{{$event->id}}"><h1>{{ $event->title }}</h1></a></div>
+                        <div class="flex__item"><span class="icon icon-calendar"></span><span>{{ $event->created_at }}</span></div>
+                        <div class="flex__item"><span class="icon icon-price-tag"></span>{{ $event->tags }}</div>
+                        <div class="flex__item"><p>{{ str_limit($event->content, 250) }}</p></div>
+                        <div class="flex__item"><a href="#" class="button">Read More</a></div>
+                    </article>
+                </div>
+            @endforeach
+        </div>
     </div>
 
 @endsection
