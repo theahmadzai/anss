416a76457e6ffdef95c2fb525faa32eeb92f9bbf
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-04 05:42:45 +0500

    add factory

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Event::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(6),
-        'tags' => '2018, International Event',
+        'venue' => $faker->sentence(1),
         'content' => $faker->sentence(30)
     ];
 });
