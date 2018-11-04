c0007208ec4f92f4e0de211dabb2df23212eb3d7
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-04 05:43:08 +0500

    add

---
+++ database/migrations/2018_11_03_213747_create_manager_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateManagerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manager', function (Blueprint $table) {
+            $table->increments('id');
+            $table->boolean('appointments_allow');
+            $table->integer('appointments_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manager');
+    }
+}
