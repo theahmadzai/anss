5bb193407f62b5ac8cf9dbdbbac58092a0020925
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-04 05:43:14 +0500

    add

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             NewsTableSeeder::class,
-            EventsTableSeeder::class
+            EventsTableSeeder::class,
+            AppointmentsTableSeeder::class,
+            ManagerTableSeeder::class
         ]);
     }
 }
