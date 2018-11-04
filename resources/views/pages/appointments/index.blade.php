0b35687943abec08f1be349f574099ded0ca3ee6
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-04 05:43:39 +0500

    update

--- resources/views/pages/appointments/index.blade.php
+++ resources/views/pages/appointments/index.blade.php
@@ -10,7 +10,42 @@
 
     <div class="container">
         <div class="container__narrow"></div>
-        <div class="container__wide"></div>
+        <div class="container__wide">
+            @if ($count > 0)
+                <p style="margin:2rem 0; text-align:center;"><strong>{{ $count }}</strong> appointments available</p>
+                <button style="padding: 0.7rem; font-size:1rem; background:white; border:1px solid #ccc; cursor:pointer; margin: 2rem auto; display:block;">Book an appointment</button>
+
+                <div style="padding:2rem 4rem;">
+                    <form action="">
+                        <div class="form__control">
+                            <label for="">Name <span>*</span></label>
+                            <input type="text">
+                        </div>
+                        <div class="form__control">
+                            <label for="">E-mail Address <span>*</span></label>
+                            <input type="email">
+                        </div>
+                        <div class="form__control">
+                            <label for="">phone <span>*</span></label>
+                            <input type="tel">
+                        </div>
+                        <div class="form__control">
+                            <label for="">Message <span>*</span></label>
+                            <textarea name=""></textarea>
+                        </div>
+                        <div class="form__control">
+                            <div class="g-recaptcha" data-sitekey="6LeAdXgUAAAAAGHqwygP-UbVQzvzNoRjXDLnCCwo"></div>
+                        </div>
+                        <div class="form__control">
+                            <button class="button">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            @else
+                <h1>No appointments</h1>
+            @endif
+
+        </div>
     </div>
 
 @endsection
