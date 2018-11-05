17e1f29f9cb1c11d29e74a2fa691af0b0556c1ff
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-06 03:21:42 +0500

    appointments

--- resources/views/pages/appointments/index.blade.php
+++ resources/views/pages/appointments/index.blade.php
@@ -12,6 +12,41 @@
         <div class="container__narrow"></div>
         <div class="container__wide">
             @if ($count > 0)
+                <div class="appointments">
+                    <div class="appointments__item">
+                        <div class="datetime">
+                            <p>Sunday 2:35 PM - 2 November, 2018</p>
+                        </div>
+                        <div class="book">
+                            <button>Book</button>
+                        </div>
+                    </div>
+                    <div class="appointments__item">
+                        <div class="datetime">
+                            <p>Sunday 4:25 PM - 2 November, 2018</p>
+                        </div>
+                        <div class="book">
+                            <p>Already Booked</p>
+                        </div>
+                    </div>
+                    <div class="appointments__item">
+                        <div class="datetime">
+                            <p>Sunday 6:00 PM - 2 November, 2018</p>
+                        </div>
+                        <div class="book">
+                            <button>Book</button>
+                        </div>
+                    </div>
+                    <div class="appointments__item">
+                        <div class="datetime">
+                            <p>Sunday 8:00 PM - 2 November, 2018</p>
+                        </div>
+                        <div class="book">
+                            <button>Book</button>
+                        </div>
+                    </div>
+                </div>
+
                 <p style="margin:2rem 0; text-align:center;"><strong>{{ $count }}</strong> appointments available</p>
                 <button style="padding: 0.7rem; font-size:1rem; background:white; border:1px solid #ccc; cursor:pointer; margin: 2rem auto; display:block;">Book an appointment</button>
 
