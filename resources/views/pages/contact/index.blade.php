418cf86c39aa987dd6b3ee72d34823cdf7a55c74
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-09 03:00:04 +0500

    update

--- resources/views/pages/contact/index.blade.php
+++ resources/views/pages/contact/index.blade.php
@@ -17,30 +17,53 @@
 
                 <p>Thank you for using this form to leave us a message. Required fields marked with *.</p>
 
-                <form action="">
+                <form method="POST" action="/contact" enctype="multipart/form-data">
+                    @csrf
+
                     <div class="form__control">
                         <label for="">Name <span>*</span></label>
-                        <input type="text">
+                        <input type="text" name="name" value="{{ old('name') }}">
+                         @if ($errors->has('name'))
+                            <p>{{ $errors->first('name') }}</p>
+                        @endif
                     </div>
+
                     <div class="form__control">
                         <label for="">E-mail Address <span>*</span></label>
-                        <input type="email">
+                        <input type="email" name="email" value="{{ old('email') }}">
+                        @if ($errors->has('email'))
+                            <p>{{ $errors->first('email') }}</p>
+                        @endif
+
                     </div>
                     <div class="form__control">
                         <label for="">Subject <span>*</span></label>
-                        <input type="text">
+                        <input type="text" name="subject" value="{{ old('subject') }}">
+                        @if ($errors->has('subject'))
+                            <p>{{ $errors->first('subject') }}</p>
+                        @endif
                     </div>
+
                     <div class="form__control">
                         <label for="">Message <span>*</span></label>
-                        <textarea name=""></textarea>
+                        <textarea name="message">{{ old('message') }}</textarea>
+                        @if ($errors->has('message'))
+                            <p>{{ $errors->first('message') }}</p>
+                        @endif
                     </div>
+
                     <div class="form__control">
                         <label for="">Files </label>
-                        <input type="file" multiple>
+                        <input type="file" name="attachments[]" multiple>
                     </div>
+
                     <div class="form__control">
-                        <div class="g-recaptcha" data-sitekey="6LeAdXgUAAAAAGHqwygP-UbVQzvzNoRjXDLnCCwo"></div>
+                        <div class="g-recaptcha" data-sitekey="{{env('NOCAPTCHA_SITEKEY')}}"></div>
+                        @if ($errors->has('g-recaptcha-response'))
+                            <p>{{ $errors->first('g-recaptcha-response') }}</p>
+                        @endif
                     </div>
+
                     <div class="form__control">
                         <button class="button">Submit</button>
                     </div>
