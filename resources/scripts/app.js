a5dfc8e6052de739bf3f94eae964bef1d9425fd5
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-09 00:51:18 +0500

    add dom ready

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,13 +1,16 @@
 import './slider'
 
-$.ajaxSetup({
-  headers: {
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-  }
-})
+$(document).ready(() => {
 
-$('#top-line').css({
-  width: '100%',
-  height: '2.5px',
-  backgroundImage: 'linear-gradient(to right, black, red, green)'
-})
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  })
+
+  $('#top-line').css({
+    width: '100%',
+    height: '2.5px',
+    backgroundImage: 'linear-gradient(to right, black, red, green)'
+  })
+});
