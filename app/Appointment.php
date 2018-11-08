d1624cfab7e0d9b00647144c0d5c5da376d8a75b
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-08 19:50:26 +0500

    update model

--- app/Appointment.php
+++ app/Appointment.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
+use Carbon\Carbon;
 
 class Appointment extends Model
 {
     protected $table = 'appointments';
+
+    public static function availableAppointments()
+    {
+        return self::where('status',false)->whereDate('timing', '>=', DB::raw('NOW()'))->get();
+    }
+
+    public function getTimingAttribute($value)
+    {
+        return Carbon::parse($value);
+    }
 }
