9e08c8f0f03c2bb4ba5a52fc297116737952f95d
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-09 02:59:51 +0500

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Resources\Json\Resource;
+use GuzzleHttp\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,18 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Resource::withoutWrapping();
+        Validator::extend('recaptcha', function($attribute, $value, $params, $validator) {
+            $client = new Client();
+
+            $response = $client->post('https://www.google.com/recaptcha/api/siteverify', [
+                'form_params' => [
+                    'secret' => env('NOCAPTCHA_SECRET'),
+                    'response' => $value
+                ]
+            ]);
+
+            return json_decode((string)$response->getBody())->success;
+        });
     }
 
     /**
