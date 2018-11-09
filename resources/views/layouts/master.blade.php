4484b932243f9ce6eb518b518f12e5cb140a3a16
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-10 03:25:45 +0500

    updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title') | {{ config('app.name') }}</title>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
         <div class="wrapper">
@@ -19,6 +19,6 @@
 
         <script src='https://www.google.com/recaptcha/api.js' async defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="/js/app.js"></script>
     </body>
 </html>
