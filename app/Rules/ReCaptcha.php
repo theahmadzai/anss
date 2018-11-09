ea8e404ff8f4b47cedd352660e96f47dae34ad53
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-09 23:56:50 +0500

    validation rule

---
+++ app/Rules/ReCaptcha.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use GuzzleHttp\Client;
+
+class ReCaptcha implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $client = new Client();
+
+        $response = $client->post('https://www.google.com/recaptcha/api/siteverify', [
+            'form_params' => [
+                'secret' => env('NOCAPTCHA_SECRET'),
+                'response' => $value
+            ]
+        ]);
+
+        return (boolean)json_decode($response->getBody())->success;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return trans('validation.recaptcha');
+    }
+}
