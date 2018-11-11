4ccc4cbd7ee781d6687692a527b5564a2e763a38
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-11 07:19:38 +0500

    add relation

--- app/Image.php
+++ app/Image.php
@@ -9,6 +9,11 @@ class Image extends Model
 {
     protected $table = 'images';
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+
     public function getUrlAttribute($url)
     {
         return Storage::url($url ?? 'default/image.png');
