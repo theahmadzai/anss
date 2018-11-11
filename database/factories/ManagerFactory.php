167bce732a8c642570a5664262a62861c14968f5
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-11 07:19:50 +0500

    change

--- database/factories/ManagerFactory.php
+++ database/factories/ManagerFactory.php
@@ -4,7 +4,5 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Manager::class, function (Faker $faker) {
     return [
-        'appointments_allow' => false,
-        'appointments_count' => 0
     ];
 });
