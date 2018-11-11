5e3f11de5bee70e5c5b4ba825ff6651c9529454d
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-11 06:51:01 +0500

    models + url method

--- app/Event.php
+++ app/Event.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Storage;
 
 class Event extends Model
 {
     protected $table = 'events';
+
+    public function getImageAttribute($image)
+    {
+        return Storage::url($image ?? 'default/image.png');
+    }
 }
--- app/News.php
+++ app/News.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Storage;
 
 class News extends Model
 {
     protected $table = 'news';
+
+    public function getImageAttribute($image)
+    {
+        return Storage::url($image ?? 'default/image.png');
+    }
 }
