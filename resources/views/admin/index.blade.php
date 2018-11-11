8f466a608fb7bfa0a6b6817c3945b70aa04b4700
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-11 19:36:51 +0500

    update index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,6 +4,8 @@
 
     <div style="padding:2rem;">
 
+        <a href="/admin/categories">Categories</a>
+        <br>
         <a href="/admin/images">Images</a>
         <br>
         <a href="/admin/appointments">Appointments</a>
