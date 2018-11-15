9559df688c418e1e46c3a13e279ad219bd0f6e4f
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-15 05:45:41 +0500

    navbar

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -2,15 +2,24 @@ import './slider'
 
 $(document).ready(() => {
 
+  // CSRF token
   $.ajaxSetup({
     headers: {
       'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
   })
 
+  // Top line
   $('#top-line').css({
     width: '100%',
     height: '2.5px',
     backgroundImage: 'linear-gradient(to right, black, red, green)'
   })
-});
+
+  // Navbar toggle
+  $('#toggle').click(event => {
+    $('#toggle').toggleClass('toggle--change')
+    $('#navbar').slideToggle()
+  })
+
+})
