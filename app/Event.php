9bbd44f5fb927d6c24815b8ee3a3a16b2fe58ae2
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-17 02:25:00 +0500

    updated

--- app/Event.php
+++ app/Event.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Storage;
+use Carbon\Carbon;
 
 class Event extends Model
 {
@@ -12,5 +13,20 @@ class Event extends Model
     public function getImageAttribute($image)
     {
         return Storage::url($image ?? 'default/image.png');
+    }
+
+    public function getDateAttribute($value)
+    {
+        return Carbon::parse($value);
+    }
+
+    public static function getPastEvents()
+    {
+        return self::whereDate('date', '<=', Carbon::now())->get();
+    }
+
+    public static function getUpcomingEvents()
+    {
+        return self::whereDate('date', '>=', Carbon::now())->get();
     }
 }
