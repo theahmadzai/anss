b9d8ee76badec8107da5cd5100026fc78d2e048d
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-23 03:04:48 +0500

    add provider observer

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\ObserverServiceProvider::class,
 
     ],
 
