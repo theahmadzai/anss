f4d34737f02fcf3ceb8fb49357b7bbe0a4aaa3b9
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-23 07:01:03 +0500

    update time to date

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Appointment::class, function (Faker $faker) {
     return [
-        'timing' => $faker->dateTimeBetween('now', '+ 5 days'),
+        'date' => $faker->dateTimeBetween('now', '+ 5 days'),
         'description' => $faker->sentence(10)
     ];
 });
