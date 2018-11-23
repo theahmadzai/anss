929ca06ced27f976280a688d5b1af3e1f2714f3a
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-23 06:58:36 +0500

    rename image to imagetool

--- config/app.php
+++ config/app.php
@@ -210,7 +210,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        'ImageTool' => Intervention\Image\Facades\Image::class
     ],
 
 ];
