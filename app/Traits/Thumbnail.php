f57417adc25258801a073c565260225e570ee3ca
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-24 08:51:45 +0500

    trait update

--- app/Traits/Thumbnail.php
+++ app/Traits/Thumbnail.php
@@ -3,23 +3,23 @@
 namespace App\Traits;
 
 use ImageTool;
-use Storage;
 
 trait Thumbnail
 {
     public function thumbnail()
     {
-        if(!$this->getOriginal('image')) {
+        if (!$this->getOriginal('image')) {
             return $this->image;
         }
 
-        return Storage::url('thumbnails/' . basename($this->image));
+        return '/storage/thumbnails/' . basename($this->image);
     }
 
-    public function createThumbnail($image, $x = 300, $y = 150) {
-        ImageTool::make($image->getRealPath())->fit($x, $y, function($constraint) {
+    public function createThumbnail($x = 300, $y = 150)
+    {
+        ImageTool::make(public_path($this->image))->fit($x, $y, function ($constraint) {
             $constraint->upsize();
             $constraint->aspectRatio();
-        })->save(public_path('/storage/thumbnails/'.basename($this->image), 80));
+        })->save(public_path('/storage/thumbnails/' . basename($this->image), 80));
     }
 }
