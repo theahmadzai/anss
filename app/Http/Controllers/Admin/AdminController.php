3249ba0b6b8668d125167407ba973ceb9d2adec5
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-24 08:49:53 +0500

    clean

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class AdminController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['auth.basic', 'admin']);
+    }
+
+    public function index()
+    {
+        return view('admin.index');
+    }
+
+}
