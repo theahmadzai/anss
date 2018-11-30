1c60a606dfd5d92e2b9b26f186d7fbb2064baadc
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-11-30 22:22:30 +0500

    update

--- app/Http/Requests/StoreSlideRequest.php
+++ app/Http/Requests/StoreSlideRequest.php
@@ -16,7 +16,7 @@ class StoreSlideRequest extends FormRequest
         return [
             'title' => 'required|min:5|max:50',
             'link' => 'nullable|url',
-            'image' => ($this->method() == 'PUT' ? '' : 'required|') . 'dimensions:min_height=300,min_width=900,max_height=400,max_width=1100,ratio=20/7',
+            'image' => ($this->method() == 'PUT' ? '' : 'required|') . 'dimensions:min_height=300,min_width=900,max_height=400,max_width=1100',
         ];
     }
 }
