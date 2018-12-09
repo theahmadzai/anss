163e0b2e25cbc3d37a27fc22a1c037dbb14b8965
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-12-09 09:36:28 +0500

    update

--- app/Http/Controllers/Admin/AppointmentController.php
+++ app/Http/Controllers/Admin/AppointmentController.php
@@ -15,37 +15,11 @@ class AppointmentController extends Controller
         ]);
     }
 
-    public function create()
-    {
-        return view('admin.appointments.create');
-    }
-
-    public function store(StoreAppointmentRequest $request)
-    {
-        Appointment::create($request->validated());
-
-        return back()->with('status', 'Appointment Scheduled Successfully!');
-    }
-
     public function show(Appointment $appointment)
     {
         return view('admin.appointments.show', [
             'appointment' => $appointment,
         ]);
-    }
-
-    public function edit(Appointment $appointment)
-    {
-        return view('admin.appointments.edit', [
-            'appointment' => $appointment,
-        ]);
-    }
-
-    public function update(StoreAppointmentRequest $request, Appointment $appointment)
-    {
-        Appointment::find($appointment->id)->update($request->validated());
-
-        return back()->with('status', 'Appointment Updated Successfully!');
     }
 
     public function destroy(Appointment $appointment)
