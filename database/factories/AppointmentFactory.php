7bc8e9f665d7772be0fd83adcc51182a1ced2a3a
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-12-09 09:37:08 +0500

    update factory

--- database/factories/AppointmentFactory.php
+++ database/factories/AppointmentFactory.php
@@ -1,10 +1,13 @@
 <?php
 
 use Faker\Generator as Faker;
-
 $factory->define(App\Appointment::class, function (Faker $faker) {
     return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'phone' => $faker->tollFreePhoneNumber,
+        'category' => $faker->biasedNumberBetween(1, 5),
+        'message' => $faker->text,
         'date' => $faker->dateTimeBetween('now', '+ 5 days'),
-        'description' => $faker->sentence(10)
     ];
 });
