94ab1704425f7a9a2448844e1c01d4ed83f673da
Javed Ahmadzai <theahmadzai@hotmail.com>
2018-12-12 01:33:19 +0500

    fixed

--- app/User.php
+++ app/User.php
@@ -5,16 +5,28 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Storage;
+use Carbon\Carbon;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $table = 'users';
+
+    protected $guarded = [];
 
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function getAvatarAttribute($value)
+    {
+        return Storage::url($value ?? 'default/image.png');
+    }
+
+    public function getDobAttribute($value)
+    {
+        return Carbon::parse($value);
+    }
 }
