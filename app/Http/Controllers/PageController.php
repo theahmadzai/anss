597a8443b657cfb4fa4198e888ad81fbeddffdb2
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-01-14 04:42:47 +0500

    limit to 3

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -23,7 +23,7 @@ class PageController extends Controller
     public function index()
     {
         $slides = Slide::get();
-        $latest_news = News::latest()->limit(9)->get();
+        $latest_news = News::latest()->limit(3)->get();
         $upcoming_events = Event::upcoming()->latest()->limit(3)->get();
 
         return view('pages.index', [
