19a17ed79d264593b7e31fd5cbfb52709841a34c
Javed Ahmadzai <theahmadzai@hotmail.com>
2019-07-05 15:24:58 +0500

    fromatted + minified

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -5,19 +5,19 @@ $(document).ready(() => {
     headers: {
       'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
-  })
+  });
 
   // Top line
   $('#top-line').css({
     width: '100%',
     height: '2.5px',
     backgroundImage: 'linear-gradient(to right, black, red, green)'
-  })
+  });
 
   // Navbar toggle
   $('#toggle').click(event => {
-    $('#toggle').toggleClass('toggle--change')
-    $('#navbar').slideToggle()
-  })
+    $('#toggle').toggleClass('toggle--change');
+    $('#navbar').slideToggle();
+  });
 
-})
+});
