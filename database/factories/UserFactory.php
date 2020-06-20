c3638c395043cbba1749df35c2a47a82fafa5ec8
Javed Ahmadzai <theahmadzai@hotmail.com>
2020-06-20 19:11:27 +0500

    fix role

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,7 +7,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => 'tester@test.com',
         'email_verified_at' => now(),
-        'role' => 4,
+        'role' => 3,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
     ];
