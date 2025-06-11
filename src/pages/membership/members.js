2f3352e0099d8436d9a37d2824aa6f6c9e674a84
Iqbal Mashal <151959357+IqbalMashal@users.noreply.github.com>
2025-06-11 15:36:48 -0400

    members page changes

--- src/pages/membership/members.js
+++ src/pages/membership/members.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { List, Avatar } from 'antd'
+import { Card, Avatar, Col, Row, Tag, Typography } from 'antd'
 import { graphql } from 'gatsby'
 import SEO from '../../components/seo'
 import Layout from '../../components/layout'
 import PageHeader from '../../components/page-header'
+
+const { Title } = Typography
 
 export const query = graphql`
   query {
@@ -20,6 +22,12 @@ export const query = graphql`
   }
 `
 
+const tierColors = {
+  Gold: 'gold',
+  Silver: 'silver',
+  Bronze: '#cd7f32',
+}
+
 const MembersPage = ({
   data: {
     allContentfulMember: { nodes: members },
@@ -29,19 +37,28 @@ const MembersPage = ({
     <Layout>
       <PageHeader title="Current Members" />
 
-      <List
-        size="large"
-        dataSource={members}
-        renderItem={member => (
-          <List.Item key={member.id}>
-            <List.Item.Meta
-              avatar={<Avatar src={member.image.url} />}
-              title={member.name}
-              description={member.tier}
-            />
-          </List.Item>
-        )}
-      />
+      <Row gutter={[24, 24]} justify="start" style={{ padding: '24px' }}>
+        {members.map(member => (
+          <Col key={member.id} xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              style={{ borderRadius: '12px', textAlign: 'center' }}
+              cover={
+                <Avatar
+                  size={100}
+                  src={member.image.url}
+                  style={{ margin: '24px auto 12px' }}
+                />
+              }
+            >
+              <Title level={4} style={{ marginBottom: 8 }}>
+                {member.name}
+              </Title>
+              <Tag color={tierColors[member.tier] || 'blue'}>{member.tier} Member</Tag>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Layout>
   )
 }
